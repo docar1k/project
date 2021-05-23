0d28c5166e8d01706c5b2b3e8fbc4c0234db7a82
docarik <docarik@gmail.com>
2021-05-23 19:24:00 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,48 @@
+'use strict';
+
+// const result = confirm('are u here?');
+// console.log(result);
+
+// const answer  = +prompt('Вам есть 18?', '10');
+// console.log(answer + 5);
+
+// const answers = [];
+
+// answers[0] = prompt('Как ваше имя?','');
+// answers[1] = prompt('Как ваша фамилия?','');
+// answers[2] = +prompt('Сколько вам лет?','');
+
+
+// document.write(typeof(null));
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5/`);
+
+// const user = 'ivan';
+
+// alert(`Привет, ${user}`)
+
+console.log('arr' + ' - obj');
+console.log(4 + ' - obj');
+
+
+let incr = 10,
+    decr = 10;
+
+// ++incr;
+// --decr;
+
+console.log(++incr);
+console.log(--decr);
+
+
+console.log(5 % 2);
+
+console.log(2 + 2 * 2 !== '6');
+
+
+const isCheked = false,
+    isClose = false;
+
+// console.log(!isCheked && !isClose );
