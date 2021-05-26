03485785a07dfb7f52372874c5eac83f7256f448
docarik <docarik@gmail.com>
2021-05-26 23:02:58 +0700

    conditions

--- js/script.js
+++ js/script.js
@@ -1,69 +1,38 @@
 'use strict';
 
-// const result = confirm('are u here?');
-// console.log(result);
-
-// const answer  = +prompt('Вам есть 18?', '10');
-// console.log(answer + 5);
-
-// const answers = [];
-
-// answers[0] = prompt('Как ваше имя?','');
-// answers[1] = prompt('Как ваша фамилия?','');
-// answers[2] = +prompt('Сколько вам лет?','');
+// if (4 == 9) {
+//     console.log('ok!');
+// } else {
+//     console.log('Error');
+// }
 
 
-// document.write(typeof(null));
 
-// const category = 'toys';
+// if (num < 49){
+//     console.log('Error');
+// }else if  (num > 100) {
+//     console.log('Много');
+// }else{
+//     console.log('ok!');
+// }
 
-// console.log(`https://someurl.com/${category}/5/`);
-
-// const user = 'ivan';
-
-// alert(`Привет, ${user}`)
-
-console.log('arr' + ' - obj');
-console.log(4 + ' - obj');
+// (num == 50) ? console.log('ok') : console.log('Error');
 
 
-let incr = 10,
-    decr = 10;
+const num = 50;
 
-// ++incr;
-// --decr;
+switch (num) {
+    case 49:
+        console.log('неверно');
+        break;
+    case 100:
+        console.log('неверно');
+        break;
+    case 51:
+        console.log('В точку!');
+        break;
 
-console.log(++incr);
-console.log(--decr);
-
-
-console.log(5 % 2);
-
-console.log(2 + 2 * 2 !== '6');
-
-
-const isCheked = false,
-    isClose = false;
-
-// console.log(!isCheked && !isClose );
-
-
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
-
-let a = prompt('Один из просмотренных фильмов', 'Терминатор'),
-    b = +prompt('На сколько оцените его?', '7'),
-    c = prompt('Один из просмотренных фильмов', 'Властелин колец'),
-    d = +prompt('На сколько оцените его?', '9.6');
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-
-console.log(personalMovieDB);
+    default:
+        console.log('не в этот раз');
+        break;
+}
