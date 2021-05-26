e1a1215cc054576262af321ebda74237fe8cf8dc
docarik <docarik@gmail.com>
2021-05-26 22:01:05 +0700

    add obj from quetion

--- js/script.js
+++ js/script.js
@@ -46,3 +46,24 @@ const isCheked = false,
     isClose = false;
 
 // console.log(!isCheked && !isClose );
+
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+let a = prompt('Один из просмотренных фильмов', 'Терминатор');
+let b = +prompt('На сколько оцените его?', '7');
+let c = prompt('Один из просмотренных фильмов', 'Властелин колец');
+let d = +prompt('На сколько оцените его?', '9.6');
+
+personalMovieDB.movies[a]=b;
+personalMovieDB.movies[c]=d;
+
+console.log(personalMovieDB);
